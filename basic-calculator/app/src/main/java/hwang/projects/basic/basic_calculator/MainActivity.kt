05e21d128fe7dc34190190b4266ef0a93706ce02
ファン <syhwang.web@gmail.com>
2021-11-26 01:33:59 +0900

    #Calculator

---
+++ basic-calculator/app/src/main/java/hwang/projects/basic/basic_calculator/MainActivity.kt
@@ -0,0 +1,25 @@
+package hwang.projects.basic.basic_calculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun ButtonClicked(v: View) {
+
+    }
+
+    fun resultButtonClicked(v: View) {
+
+    }
+
+    fun clearButtonClicked(v: View) {
+
+    }
+
+}
